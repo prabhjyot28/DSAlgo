291866174fae1f5384cbfc15b88c365b9e286284
Prabhjyot Singh <tryyourrajat@gmail.com>
2020-04-09 20:21:45 +0530

    Union Find

---
+++ UnionFind.py
@@ -0,0 +1,36 @@
+
+class UnionFind:
+    def __init__(self):
+        self.id = {}
+        self.sz = {}
+        self.count = 0
+        return
+
+    def add(self, p):
+        self.id[p] = p
+        self.sz[p] = 1
+        self.count += 1
+
+    def root(self, i):
+        node = i
+        while i != self.id[i]:
+            self.id[i] = self.id[self.id[i]]
+            i = self.id[i]
+
+        # Path Compression.
+        # while node!=i:
+        #     next = self.id[node]
+        #     self.id[node] = i
+        #     node = next
+        
+        return i
+
+    def unite(self, p, q):
+        i, j = self.root(p), self.root(q)
+        if i == j:
+            return
+        if self.sz[i] > self.sz[j]:
+            i, j = j, i
+        self.id[i] = j
+        self.sz[j] += self.sz[i]
+        self.count -= 1
