ef1be3324fc4dd215be819d1197811809d09cb90
Prabhjyot Singh <tryyourrajat@gmail.com>
2020-04-09 19:03:15 +0530

    Adding Bucket Sort

---
+++ Avl Trees.py
@@ -0,0 +1,16 @@
+# Balanced Binary search trees are those having guraanted height of O(logn)
+
+#AVL Tree is one of type of BBST.
+# Balance Factor (root ) = Height of right tree  - Height of left tree.
+# AVL trees balance themselves by rotation.
+#
+# Inserting into an AVL tree.
+# Removal is similar to BST. (find the node and replace it with its scucessor).
+
+
+# Red Black trees is also kind of BBST.
+# Node is either red or black.
+# Root and (Nill Nodes) are always black.
+# If a node is red then its children are black.
+# All paths from a node to its nill descendants conatins same number of black nodes.
+# The longest path (root to farthest Nil) is no more than twice the length of shortest path.(root to nearest Nil)
--- BinaryIndexedTree.py
+++ BinaryIndexedTree.py
@@ -2,7 +2,7 @@
 
 class BIT:
     """Implementation of a Binary Indexed Tree (Fennwick Tree)"""
-    
+
     #def __init__(self, list):
     #    """Initialize BIT with list in O(n*log(n))"""
     #    self.array = [0] * (len(list) + 1)
@@ -52,7 +52,7 @@ class SegmentTree:
     def __init__(self, values):
         self.data = [0 for _ in values] + values
         self.n = len(values)
-        
+
         for idx in reversed(range(1, self.n)):
             self.data[idx] = min(self.data[2*idx], self.data[2*idx+1])
 
@@ -80,3 +80,50 @@ class SegmentTree:
             right //= 2
 
         return minimum
+
+
+# Leetcode 220.
+#If it reminds us of doing a range check which is implemented in tree data structure
+# and would take O(LogN) if a balanced tree structure is used, or doing a bucket
+# check which is constant time. We shall just discuss the idea using bucket here.
+# Bucketing means we map a range of values to the a bucket. For example, if the bucket
+# size is 3, we consider 0, 1, 2 all map to the same bucket.
+# However, if t == 3, (0, 3) is a considered duplicates but does not map to the same bucket.
+# This is fine since we are checking the buckets immediately before and after as well.
+# So, as a rule of thumb, just make sure the size of the bucket is reasonable such that
+# elements having the same bucket is immediately considered duplicates or duplicates must
+# lie within adjacent buckets. So this actually gives us a range of possible bucket size,
+# i.e. t and t + 1. We just choose it to be t and a bucket mapping to be num / t.
+
+
+
+# Bucket Sort
+# 1) Create n empty buckets (Or lists).
+# 2) Do following for every array element arr[i].
+# .......a) Insert arr[i] into bucket[n*array[i]]
+# 3) Sort individual buckets using insertion sort.
+# 4) Concatenate all sorted buckets.
+
+def bucketSort(x):
+    arr = []
+    slot_num = 10 # 10 means 10 slots, each
+                  # slot's size is 0.1
+    for i in range(slot_num):
+        arr.append([])
+
+    # Put array elements in different buckets
+    for j in x:
+        index_b = int(slot_num * j)
+        arr[index_b].append(j)
+
+    # Sort individual buckets
+    for i in range(slot_num):
+        arr[i].sort()
+
+    # concatenate the result
+    k = 0
+    for i in range(slot_num):
+        for j in range(len(arr[i])):
+            x[k] = arr[i][j]
+            k += 1
+    return x
