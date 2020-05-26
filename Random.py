ffa8371867e682641c6ec5ca2533d3f8fe5c22af
Prabhjyot Singh <tryyourrajat@gmail.com>
2020-05-26 12:28:55 +0530

    Random

---
+++ Random.py
@@ -0,0 +1,6 @@
+from random import *
+randrange(start[opt], stop, step[opt])    # return a random number between [start, start+1.......stop-1]
+randint(start, stop)                      # returns a random int between [start, start+1...........stop]
+choice(seq)                               # return a random element from given sequence.
+shuffle(seq)                              # returns a seq in random order.
+random()                                  # returns a floating number between [0.0, 1.0)
